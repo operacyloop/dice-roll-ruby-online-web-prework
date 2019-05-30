6c06d0f2b533094ce30da6a70f83a2a92b501c7d
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-05-30 19:16:18 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,7 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  dice_roll = rand(6) + 1
+  dice_roll = rand(1..6)
   puts Integer(dice_roll)
   return Integer(dice_roll)
 end
